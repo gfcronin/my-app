3e06ad4c0c801572d9c9d30113ad357765bc8407
gfcronin <garry.cronin@gmail.com>
2020-06-20 00:23:39 +0100

    initial versions

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,6 +7,7 @@ package com.mycompany.app;
 public class App 
 {
     // this is just a test
+    // it's a simple test
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
